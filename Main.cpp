b42386f58785ec42fb1dbaff1b6845116ee784cf
Hollander-Lawn <hoonyikonnalexander@outlook.com>
2024-07-20 13:12:25 +0800

    Create Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+  cout << "Howdy stranger\n";
+  cout << "welcome to the Tin Tin geoup chat\n";
+  cout << "Have a nice day\n";
+  cout << "Please read README.md for furthur clarification on rules\n";
+}
