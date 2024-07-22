7131244cb33f67b4bfabed82188461907b0ca05f
Hollander-Lawn <hoonyikonnalexander@outlook.com>
2024-07-22 20:38:41 +0800

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -7,4 +7,5 @@ int main(){
   cout << "welcome to the Tin Tin geoup chat\n";
   cout << "Have a nice day\n";
   cout << "Please read README.md for furthur clarification on rules\n";
+  cout << "Please read latest release Alpha 0.1.0\n";
 }
